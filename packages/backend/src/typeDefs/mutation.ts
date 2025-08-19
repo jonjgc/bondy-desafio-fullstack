101b74b5f836cd3f308df412f433a78d72f08a75
Jonatas <jonatasmc04@gmail.com>
2025-08-19 15:35:57 -0300

    feat(backend): Add login mutation schema

--- packages/backend/src/typeDefs/mutation.ts
+++ packages/backend/src/typeDefs/mutation.ts
@@ -3,5 +3,6 @@ import gql from 'graphql-tag'
 export default gql`
   type Mutation {
     mutationTest(test: Boolean): Boolean
+    login(email: String!, password: String!): User
   }
 `
--- packages/backend/src/typeDefs/query.ts
+++ packages/backend/src/typeDefs/query.ts
@@ -1,6 +1,12 @@
 import gql from 'graphql-tag'
 
 export default gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+  }
+
   type Query {
     queryTest: Boolean
   }
